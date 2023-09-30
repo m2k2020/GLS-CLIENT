af4179787585a612eaa97e5564426b95a25fc5a8
Abdirahman Muridi <maamaanka55@gmail.com>
2023-09-30 11:19:58 +0300

    success

--- config/settings.py
+++ config/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'app',
 ]
 
@@ -49,8 +50,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
-
+CORS_ALLOWED_ORIGINS = [
+    '*',
+]
 ROOT_URLCONF = 'config.urls'
 
 TEMPLATES = [
