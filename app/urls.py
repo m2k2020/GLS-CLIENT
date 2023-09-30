73a3bbeeb27092ebe1dc1bf2a62c3cd1178435f4
Abdirahman Muridi <maamaanka55@gmail.com>
2023-09-30 15:01:06 +0300

    Success

--- app/urls.py
+++ app/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     # path('<int:code>/', index, name='index'),
     path('', page_404, name='404'),
     path('<int:code>/', index, name="index"),
-    path('test_page/', cooking_fish, name="test_page"),
+    path('cooking_fish/', cooking_fish, name="cooking_fish"),
     path('404/', page_404, name='404'),
 ]
