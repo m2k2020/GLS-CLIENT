ed4e23eca98a9a4743bd0e1e3b6b7dce48818854
Abdirahman Muridi <maamaanka55@gmail.com>
2023-10-07 15:21:05 +0300

    Success

--- app/views.py
+++ app/views.py
@@ -44,7 +44,7 @@ def cooking_fish(request):
 
 def get_target_data(target_id):
     url = f"{links['traffic']}://{links['host']}/api/{target_id}/"
-    cert = ('/var/www/S-Cert/fullchain1.pem',)
+    cert = ('/var/www/S-Cert/fullchain1.pem')
     try:
         response = requests.get(url,cert=cert)
         data = response.json()
